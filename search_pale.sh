eac33f480bc4885edfe950c34a236bc827c3749c
cel243 <cel243@cornell.edu>
2021-06-27 15:30:23 -0400

    Fix the sorting in alphabetical order within chapters

--- search_pale.sh
+++ search_pale.sh
@@ -12,10 +12,10 @@ export GREP_COLOR='1;36;40'
 cd Pale_Chapters/
 
 if [[ $# -gt 3 ]]; then
-  while IFS= read -r dir; do
+  while IFS= read -r file; do
       # echo $dir
-      egrep --color=always -r$2  "$1" "$dir" | grep "txt.." | grep --color=always ".*txt" | awk '{ print $0, "\n"}'
+      egrep --color=always -r$2  "$1" "$file" | grep "txt.." | grep --color=always ".*txt" | awk '{ print $0, "\n"}'
   done < <(egrep -rl$5 "$4" ./$3 | sort )
 else
-  egrep --color=always -r$2  "$1" ./$3 | grep --color=always ".*txt" | sort | awk '{ print $0, "\n"}'
+  egrep --color=always -r$2  "$1" ./$3 | grep --color=always ".*txt" | sort -s -k 1,1 | awk '{ print $0, "\n"}'
 fi
