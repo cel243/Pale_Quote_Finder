4a518bf455281ef6f9181c8de40879b880e29d64
cel243 <cel243@cornell.edu>
2021-10-11 00:05:11 -0400

    script to get a description for a chatper

---
+++ synopsis.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+
+if [[ $# -lt 1 ]]; then
+  echo "requires regex"
+fi
+
+echo " "
+
+export GREP_COLOR='1;36;40'
+
+./describe_all.sh \
+  | egrep --color=none "$1" \
+  | egrep --color=always ".*:"\
+  | awk '{ print $0, "\n"}'
+
+echo " "
+
+
